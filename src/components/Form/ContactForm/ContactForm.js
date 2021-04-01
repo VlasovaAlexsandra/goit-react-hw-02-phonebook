72eced4b0ac280259c70f67d30c2b369571c96a3
VlasovaAlexsandra <68018046+VlasovaAlexsandra@users.noreply.github.com>
2021-04-02 01:30:47 +0300

    end

--- src/components/Form/ContactForm/ContactForm.js
+++ src/components/Form/ContactForm/ContactForm.js
@@ -1,43 +1,46 @@
 import { Component } from "react";
+import PropTypes from "prop-types";
 import shortid from 'shortid';
+import './ContactForm.css';
 
-class Form extends Component {
+const initialState = {
+    name: '',
+    number: ''
+}
+
+class ContactForm extends Component {
     state = {
-        contacts: [
-            { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-            { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-            { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-        ],
-        name: '',
-        number: ''
+        ...initialState
     }
 
     nameInputId = shortid.generate();
     numberInputId = shortid.generate();
+    findInput = shortid.generate();
 
     handleChange = e => {
         const { name, value } = e.currentTarget;
-
         this.setState({ [name]: value });
 
     }
+
     handleSubmit = (e) => {
         e.preventDefault();
-        this.props.onSubmit(this.state)
+        this.props.onSubmit({ ...this.state })
         this.reset()
     }
+
     reset = () => {
-        this.setState({ name: '', number: '' })
+        this.setState({ ...initialState })
     }
+
     render() {
         return (
             <>
-                <h1>Phonebook</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <label htmlFor={this.nameInputId}>Name</label>
+                <form className="TaskEditor" onSubmit={this.handleSubmit}>
+                    <label className="TaskEditor_label" htmlFor={this.nameInputId}>Name</label>
                     <br />
                     <input
+                        className="TaskEditor_input"
                         type="text"
                         value={this.state.name}
                         name="name"
@@ -48,29 +51,30 @@ class Form extends Component {
                         id={this.nameInputId}
                     />
                     <br />
-                    <label htmlFor={this.numberInputId}>Number</label>
+                    <label className="TaskEditor_label" htmlFor={this.numberInputId}>Number</label>
                     <br />
                     <input
+                        className="TaskEditor_input"
                         type="text"
                         value={this.state.number}
                         name="number"
+                        pattern="(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})"
+                        title="Номер телефона должен состоять из 11-12 цифр и может содержать цифры, пробелы, тире, пузатые скобки и может начинаться с +"
                         required
                         onChange={this.handleChange}
                         id={this.numberInputId}
                     />
                     <br />
-                    <button type="submit">Add contact</button>
+                    <button className="TaskEditor_button" type="submit">Add contact</button>
                 </form>
 
-                <h2>Contacts</h2>
-                <ul> {this.state.contacts.map(({ id, name, number }) => (
-                    <li key={id} name={name} number={number}></li>
-                ))}
-
-                </ul>)
             </>
         )
     }
 }
 
-export default Form;
+ContactForm.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+};
+
+export default ContactForm;
